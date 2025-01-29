4ec8ed4159f73b19dbb29fa4124fa4d139a98319
shyamsah23 <92447257+shyamsah23@users.noreply.github.com>
2025-01-29 18:41:51 +0530

    added controller,model,dummyService

--- src/main/java/com/example/Student/StudentApplication.java
+++ src/main/java/com/example/Student/StudentApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudentApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(StudentApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
